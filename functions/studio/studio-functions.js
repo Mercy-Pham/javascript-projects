705ba6e2874b3a1b635ff1e56e03b2b85bfb6e26
Mercy Pham <mercypham@Mac-mini.local>
2024-01-25 20:12:23 -0600

    Studio 5 completed

--- functions/studio/studio-functions.js
+++ functions/studio/studio-functions.js
@@ -9,6 +9,16 @@
 // 5. Use console.log(reverseCharacters(myVariableName)); to call the function and verify that it correctly reverses the characters in the string.
 // 6. Optional: Use method chaining to reduce the lines of code within the function.
 
+
+// function reverseCharacters (original) {
+//     return original.split('').reverse().join('')
+    
+// }
+// let myVariableName = "LaunchCode";
+// let myVariableNumber = 1234
+// console.log(reverseCharacters(myVariableNumber));
+
+
 // Part Two: Reverse Digits
 
 // 1. Add an if statement to reverseCharacters to check the typeof the parameter.
@@ -16,6 +26,17 @@
 // 3. If typeof is ’number’, convert the parameter to a string, reverse the characters, then convert it back into a number.
 // 4. Return the reversed number.
 // 5. Be sure to print the result returned by the function to verify that your code works for both strings and numbers. Do this before moving on to the next exercise.
+
+function reverseCharacters (original) {
+    if (typeof(original) === "string") {
+        return original.split('').reverse().join('')
+    } else if (typeof(original) === "number") {
+        return Number((String(original).split('').reverse().join('')))
+    }
+}
+let myVariableName = "LaunchCode";
+let myVariableNumber = 1234
+console.log(reverseCharacters(myVariableNumber));
 
 // Part Three: Complete Reversal
 
@@ -26,9 +47,18 @@
 // 5. Return the final, reversed array.
 // 6. Be sure to print the results from each test case in order to verify your code.
 
+let emptyArray = [];
 let arrayTest1 = ['apple', 'potato', 'Capitalized Words'];
 let arrayTest2 = [123, 8897, 42, 1168, 8675309];
 let arrayTest3 = ['hello', 'world', 123, 'orange'];
+
+function completeReversal (reverseArray) {
+    for (let i = 0; i < reverseArray.length; i++) {
+        emptyArray.push(reverseCharacters(reverseArray[i]));
+    }
+}
+completeReversal(arrayTest1)
+console.log(emptyArray);
 
 // Bonus Missions
 
