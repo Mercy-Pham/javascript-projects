613541bc568ae7805d447e3bd4b149ef444a80e4
Mercy Pham <mercypham@Mac-mini.local>
2024-01-29 21:41:47 -0600

    Class 6 Studio Completed

--- more-on-functions/studio/part-three-number-sorting-easy-way.js
+++ more-on-functions/studio/part-three-number-sorting-easy-way.js
@@ -5,4 +5,26 @@ let nums3 = [200, 5, 4, 10, 8, 5, -3.3, 4.4, 0];
 
 //Sort each array in ascending order.
 
+function numberSortingArray(nums) {
+    nums.sort(function(a,b) {
+        return a - b;
+    });
+    console.log(nums);
+}
+
+numberSortingArray(nums1);
+numberSortingArray(nums2);
+numberSortingArray(nums3);
+
 //Sort each array in decending order.
+
+function numberDescendingArray(nums) {
+    nums.sort(function(b,a) {
+        return a - b;
+    });
+    console.log(nums);
+}
+
+numberDescendingArray(nums1);
+numberDescendingArray(nums2);
+numberDescendingArray(nums3);
